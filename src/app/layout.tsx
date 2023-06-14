5481b4ae5275392d7b50280f672a76e0bc93836c
FuuKowatty <miedzio.2000@gmail.com>
2023-06-14 20:07:20 +0200

    main: add posts data

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
+import Navbar from '@/components/Navbar'
 import './globals.css'
-import { Inter } from 'next/font/google'
+import { Roboto } from 'next/font/google'
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Roboto({ subsets: ["latin"], weight: "400"})
 
 export const metadata = {
   title: 'Create Next App',
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} bg-secondary text-primary`}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   )
 }
