45b7629462ca430e2ff23e368df470d46d88b726
FuuKowatty <miedzio.2000@gmail.com>
2023-06-21 23:29:25 +0200

    development: set minimum clickable area on 44x44 based on W3C in theme button

--- src/components/DarkModeButton.tsx
+++ src/components/DarkModeButton.tsx
@@ -9,13 +9,16 @@ export function DarkModeToggle() {
   }
 
   return (
-    <div className='bg-white dark:bg-darkGray  ml-auto w-[42px] h-[24px] border-gray-200 border-[1.5px] rounded-lg flex items-center justify-center p-2 relative cursor-pointer text-sm dark:border-white/10' onClick={handleChangeMode} aria-label="change-theme">
+    <span className='min-w-[44px] min-h-[44px] flex items-center'  
+      onClick={handleChangeMode}>
+    <button className='bg-white dark:bg-darkGray  ml-auto w-[42px] h-[24px] border-gray-200 border-[1.5px] rounded-lg flex items-center justify-center p-2 relative cursor-pointer text-sm dark:border-white/10'>
       <div><BsFillSunFill /></div>
       <div><BsFillMoonFill /></div>
       <div
         className={`w-[18px] h-[18px] border-[3px] border-blue-300 bg-lightBlue rounded-full absolute left-[2px] dark:left-auto right-auto dark:right-[2px] animate-open`}
       />
-    </div>
+    </button>
+    </span>
   );
 };
 
