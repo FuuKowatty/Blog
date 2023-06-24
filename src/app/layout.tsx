340dd0a40d2665d024bd05bf3cc3f7f07b54ca51
FuuKowatty <miedzio.2000@gmail.com>
2023-06-24 16:32:51 +0200

    delete animation from button link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,37 +1,25 @@
-"use client"
-
 import { Navbar } from '@/components/Navbar'
 import './globals.css'
 import Footer from '@/components/Footer'
-import { ThemeProvider } from 'next-themes'
 import { DarkModeToggle } from '@/components/Buttons/DarkModeButton'
-import { useEffect } from 'react'
+import Providers from './providers'
+
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode
 }) {
-
-
-
-  useEffect(() => {
-    document.body.classList.add('transition-colors', 'duration-300');
-    return () => {
-      document.body.classList.remove('transition-colors', 'duration-300');
-    };
-  }, []);
-
   return (
     <html lang="en"> 
       <body>
-        <ThemeProvider attribute='class' >
-          <Navbar>
-            <DarkModeToggle />
-          </Navbar>
-          {children}
-        </ThemeProvider>
-          <Footer />
+          <Providers>
+            <Navbar>
+              <DarkModeToggle />
+            </Navbar>
+            {children}
+            <Footer />
+          </Providers>
       </body>
     </html>
   )
--- src/components/Buttons/ButtonLink.tsx
+++ src/components/Buttons/ButtonLink.tsx
@@ -12,7 +12,7 @@ export function ButtonLink({href, openInNewTab, children} : ButtonProps) {
   const linkProps = openInNewTab ? { target: "_blank", rel: "noopener noreferrer" } : {};
 
   return (
-    <Link href={href} role="button" aria-label="button-header" className="p-3 bg-lightBlue/10 dark:bg-darkBlue rounded-md text-lightBlue text-base min-w-[128px] flex justify-center items-center gap-1 hover:bg-lightBlue hover:text-white dark:hover:bg-lightBlue transition-colors duration-300 ease-in-out" {...linkProps}>
+    <Link href={href} role="button" aria-label="button-header" className="p-3 bg-lightBlue/10 dark:bg-darkBlue rounded-md text-lightBlue text-base min-w-[128px] flex justify-center items-center gap-1 hover:bg-lightBlue hover:text-white dark:hover:bg-lightBlue" {...linkProps}>
         {children}
     </Link>
   )
