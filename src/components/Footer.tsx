7d33992c326aacb03407efec20fba973a880e7eb
FuuKowatty <miedzio.2000@gmail.com>
2023-11-20 00:22:28 +0100

    edit footer text --fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className='min-h-[100px] grid items-end justify-center border-t-lightGray dark:border-t-silver border-t-[1px] mt-12 px-4 text-center'>
         <span className='mb-6'>
-            © 2023 POKEWIKI. All rights reserved. Created with ❤️ by {' '}
+            © 2023 & all rights reserved. Created with ❤️ by {' '}
             <a href="https://github.com/FuuKowatty" className='underline text-lightBlue font-bold' target='_blank'>FuuKowatty</a>
             
         </span>
